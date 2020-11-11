07c34fab1d927bf160c18bfdc7c92c95d5d97369
mohamedBAF <mohamed.benabedelfattah@esprit.tn>
2020-11-11 17:03:36 +0100

    Updates

---
+++ precache-manifest.02b595b0d0345f59c0cc9061a4a49bde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53823929a6df2d04d07117c0bab70e86",
+    "url": "/mohamedBAF/Chat-Application/index.html"
+  },
+  {
+    "revision": "243717eff275a5816e20",
+    "url": "/mohamedBAF/Chat-Application/static/css/main.d74991c9.chunk.css"
+  },
+  {
+    "revision": "79fedde7d33d603d9aa1",
+    "url": "/mohamedBAF/Chat-Application/static/js/2.bad7e98d.chunk.js"
+  },
+  {
+    "revision": "297d5a0230a01f222fb67f41860ddc4b",
+    "url": "/mohamedBAF/Chat-Application/static/js/2.bad7e98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243717eff275a5816e20",
+    "url": "/mohamedBAF/Chat-Application/static/js/main.d895c25e.chunk.js"
+  },
+  {
+    "revision": "f83a0bd1c4802b52954a",
+    "url": "/mohamedBAF/Chat-Application/static/js/runtime-main.cd8cfbde.js"
+  },
+  {
+    "revision": "05ea36a54236a1eb6345defcc79d40ec",
+    "url": "/mohamedBAF/Chat-Application/static/media/favicon copy.05ea36a5.ico"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mohamedBAF/Chat-Application/static/media/logo.5d5d9eef.svg"
+  }
+]);
